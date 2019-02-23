352b369ab9e19318d788dbd6ef3e6a9e5d753f96
ani <ani.grigorian.85@gmail.com>
2019-02-22 23:34:28 -0800

    add firebase configs

---
+++ src/firebase/FireManager.js
@@ -0,0 +1,64 @@
+import { firestore } from "firebase";
+
+export default class FireManager {
+    static addStudent(student) {
+        if (student.id) {
+            return firestore()
+                .collection("students")
+                .doc(student.id)
+                .set(student);
+        }
+    }
+
+    static removeStudent(student) {
+        if (student.id) {
+            return firestore()
+                .collection("students")
+                .doc(student.id)
+                .delete();
+        }
+    }
+
+    static editStudent(student) {
+        if (student.id) {
+            return firestore()
+                .collection("students")
+                .doc(student.id)
+                .update({ ...student });
+        }
+    }
+
+    static getStudent(student) {
+        if (student.id) {
+            const ref = firestore()
+                .collection("students")
+                .doc(student.id);
+
+            return ref
+                .get()
+                .then(doc => {
+                    if (doc.exists) {
+                        return doc.data();
+                    } else {
+                        console.error("No such student!");
+                    }
+                })
+                .catch(function(error) {
+                    console.error("Error getting student:", error);
+                });
+        }
+    }
+
+    static getStudents() {
+        const studentsRef = firestore().collection("students");
+        console.log(" = ",studentsRef)
+        return studentsRef
+            .get()
+            .then(function(querySnapshot) {
+                return querySnapshot.docs.map(doc => doc.data());
+            })
+            .catch(function(error) {
+                console.error("Error getting students:", error);
+            });
+    }
+}
---
+++ src/firebase/fireConfig.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase';
+import 'firebase/firestore'
+
+
+const config = {
+    apiKey: "AIzaSyABwTokkW4fQhjSCurJJpp3S6HBCsB45IA",
+    authDomain: "aca-graduate-s-list.firebaseapp.com",
+    databaseURL: "https://aca-graduate-s-list.firebaseio.com",
+    projectId: "aca-graduate-s-list",
+    storageBucket: "aca-graduate-s-list.appspot.com",
+    messagingSenderId: "361257217865"
+};
+
+
+export  function initFirebase() {
+    // Initialize Firebase
+    firebase.initializeApp(config);
+
+    // Initialize Cloud Firestore through Firebase
+    // firebase.firestore().settings({timestampsInSnapshots: true});
+}
+
+export function createUserInfirebase(firstName, lastName, email, password) {
+    firebase.auth().createUserWithEmailAndPassword(firstName, lastName, email, password).catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // ...
+    });
+}
+export default {initFirebase, createUserInfirebase}
