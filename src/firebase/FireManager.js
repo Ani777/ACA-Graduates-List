f3a67bd8029212959b9f26efbc9dabc85c78743e
ani <ani.grigorian.85@gmail.com>
2019-02-26 09:35:17 -0800

    add firebase functions

--- src/firebase/FireManager.js
+++ src/firebase/FireManager.js
@@ -1,14 +1,81 @@
 import { firestore } from "firebase";
-import firebase from 'firebase';
+import  firebase  from 'firebase';
 
 export default class FireManager {
+
+    static  writeUserData(course, dateOfBirth, email, feedback, firstName, lastName, phone, testResults, works){
+        return firebase.writeUserData(course, dateOfBirth, email, feedback, firstName, lastName, phone, testResults, works)
+    }
+
+
+    static createStudentInFirebase(studentData, id) {
+        return firestore()
+            .collection("students")
+            .doc(id)
+            .set(studentData)
+    }
+
+    static removeStudent(id) {
+        return firestore()
+            .collection("students")
+            .doc(id)
+            .delete();
+    }
+
+    static editStudent(id, newData) {
+        return firestore()
+            .collection('students')
+            .doc(id)
+            .update({...newData})
+    }
+
     static createUserWithEmailAndPassword (email, password){
         return firebase.auth().createUserWithEmailAndPassword(email, password)
     }
 
-    static createCompanyInFirebase(companyData, id) {
+    static createCompanyInFirebase(companyData) {
         return firebase.firestore().collection("companies").add(companyData)
         .then(data =>console.log(data));
+    }
+
+    static removeCompany(id) {
+            return firestore()
+                .collection("companies")
+                .doc(id)
+                .delete();
+    }
+
+    static updateCompany(id, newData) {
+        return firestore()
+            .collection('companies')
+            .doc(id)
+            .update({...newData})
+    }
+
+    static addCourse(newCourse) {
+        firestore().collection('courses').doc('ZtsTO8Ldz0B6tCHNEHlY').get()
+            .then(doc => {
+                const courses = doc.data().courses;
+                if (!courses.includes(newCourse)) {
+                    const newCourses = [...courses];
+                    newCourses.push(newCourse);
+                    return newCourses;
+                }
+            })
+            .then(newCourses => db.collection('courses').doc('ZtsTO8Ldz0B6tCHNEHlY').update({courses: newCourses}))
+    }
+
+    static removeCourse(course) {
+       firestore().collection('courses').doc('ZtsTO8Ldz0B6tCHNEHlY').get()
+            .then(doc => {
+                const courses = doc.data().courses;
+                if (courses.includes(course)) {
+                    const newCourses = [...courses];
+                    newCourses.splice(courses.indexOf(course), 1);
+                    return newCourses;
+                }
+            })
+            .then(newCourses => db.collection('courses').doc('ZtsTO8Ldz0B6tCHNEHlY').update({courses: newCourses}))
     }
 
     // static addStudent(student) {
@@ -20,14 +87,7 @@ export default class FireManager {
     //     }
     // }
 
-    // static removeCompany(company) {
-    //     if (student.id) {
-    //         return firestore()
-    //             .collection("students")
-    //             .doc(student.id)
-    //             .delete();
-    //     }
-    // }
+
 
     // static editStudent(student) {
     //     if (student.id) {
