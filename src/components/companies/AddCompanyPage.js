d14c4a6645042a7b2157f51630ad888f75408fa5
ani <ani.grigorian.85@gmail.com>
2019-03-08 21:16:25 +0400

    fixing errors

--- src/components/companies/AddCompanyPage.js
+++ src/components/companies/AddCompanyPage.js
@@ -132,6 +132,7 @@ const styles = theme => ({
                     />
                     <div className={classes.passwordRow}>
                         <TextField
+                            value={password}
                             id="password"
                             required
                             fullWidth
