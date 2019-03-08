cf6187de10c45c4ece0d6b9ff7370a3a47c489cc
ani <ani.grigorian.85@gmail.com>
2019-03-08 18:51:52 +0400

    materialize addcourse page

--- src/components/courses/AddCoursePage.js
+++ src/components/courses/AddCoursePage.js
@@ -1,39 +1,84 @@
-import React from 'react';
+import React, { Components, useState } from 'react';
 import FireManager from '../../firebase/FireManager';
 import { useFormInput } from '../../hooks';
 //import { v4 } from 'uuid';
 import '../../App.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 //import {TextField} from '@material-ui/core/TextField';
 
-export default function AddCoursePage(props) {
+const styles = theme => ({
+    button: {
+        marginTop: '3%',
+        marginLeft: '2%',
+    },
+    input: {
+        display: 'none',
+    },
+    container: {
+        marginLeft: '37%',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+});
 
-    const name = useFormInput('');
 
+function AddCoursePage(props) {
+    const [name, setName] = useState('');
 
-
+    function handleChange(e) {
+        setName(e.target.value)
+    }
     function onCourseFormSubmit (e) {
         e.preventDefault();
-        //const id = v4();
-        const data = {
-            name: name.value
 
+        if (name) {
+
+            //const id = v4();
+            const data = {
+                name,
+            }
+            FireManager.createCourseInFirebase(data).then(() => {
+                props.addCourseToList(data);
+                setName('');
+            }).catch(err => {
+                console.error(err.message)
+            });
         }
-
-
-
-        FireManager.createCourseInFirebase(data).then(()=> {
-            props.addCourseToList(data)
-        }).catch(err=>{
-            console.error(err.message)
-
-        });
     }
 
-
+    const { classes } = props;
     return (
-        <form onSubmit={onCourseFormSubmit}>
-            <input placeholder="Course Name" {...name} />
-            <button type='submit'>Add</button>
+        <form className={classes.container} noValidate autoComplete="off" onSubmit={onCourseFormSubmit}>
+            <TextField
+                value={name}
+                onChange={handleChange}
+                required
+                autoFocus
+                label="Course Name"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                margin="normal"
+            />
+            <Button  type='submit' variant="contained" color="primary" className={classes.button}>
+                Add
+            </Button>
         </form>
     );
 }
+
+export default withStyles(styles)(AddCoursePage);
